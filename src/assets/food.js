3bae7689ff7d4e08562921d1756948695eacaae0
Danielambrose-web <danielessientech121@gmail.com>
2026-06-04 13:46:50 +0100

    changed folder directory

---
+++ src/assets/food.js
@@ -0,0 +1,248 @@
+import food_1 from "./HeaderImg.jpg";
+import food_2 from "./Ice1.jpg";
+import food_3 from "./Ice2.jpg";
+import food_4 from "./Ice3.jpg";
+import food_5 from "./Ice4.jpg";
+import food_6 from "./Juice1.jpg";
+import food_7 from "./Juice2.jpg";
+import food_8 from "./Juice3.jpg";
+import food_9 from "./Pie1.jpg";
+import food_10 from "./Pie2.jpg";
+import food_11 from "./Pie3.jpg";
+import food_12 from "./Pie4.jpg";
+import food_13 from "./Pie5.jpg";
+import food_14 from "./Rice1.jpg";
+import food_15 from "./Rice2.jpg";
+import food_16 from "./Rice3.jpg";
+import food_17 from "./Rice4.jpg";
+import food_18 from "./Rice5.jpg";
+import food_19 from "./Soup1.jpg";
+import food_20 from "./Soup2.jpg";
+import food_21 from "./Soup3.jpg";
+import food_22 from "./Soup4.jpg";
+import food_23 from "./Soup5.jpg";
+
+
+import cart from './basket_icon.png';
+import cross from './cross_icon.png';
+import add from './add_icon_green.png';
+import remove from "./remove_icon_red.png";
+
+
+export const icons = {
+  cart,
+  cross,
+  add,
+  remove,
+  food_1
+}
+
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: food_1,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    _id: "2",
+    name: "Veg salad",
+    image: food_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    _id: "3",
+    name: "Clover Salad",
+    image: food_3,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    _id: "4",
+    name: "Chicken Salad",
+    image: food_4,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Salad",
+  },
+  {
+    _id: "5",
+    name: "Lasagna Rolls",
+    image: food_5,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "6",
+    name: "Peri Peri Rolls",
+    image: food_6,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "7",
+    name: "Chicken Rolls",
+    image: food_7,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "8",
+    name: "Veg Rolls",
+    image: food_8,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "9",
+    name: "Ripple Ice Cream",
+    image: food_9,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "10",
+    name: "Fruit Ice Cream",
+    image: food_10,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "11",
+    name: "Jar Ice Cream",
+    image: food_11,
+    price: 10,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "12",
+    name: "Vanilla Ice Cream",
+    image: food_12,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "13",
+    name: "Chicken Sandwich",
+    image: food_13,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "14",
+    name: "Vegan Sandwich",
+    image: food_14,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "15",
+    name: "Grilled Sandwich",
+    image: food_15,
+    price: 16,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "16",
+    name: "Bread Sandwich",
+    image: food_16,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "17",
+    name: "Cup Cake",
+    image: food_17,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "18",
+    name: "Vegan Cake",
+    image: food_18,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "19",
+    name: "Butterscotch Cake",
+    image: food_19,
+    price: 20,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "20",
+    name: "Sliced Cake",
+    image: food_20,
+    price: 15,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "21",
+    name: "Garlic Mushroom ",
+    image: food_21,
+    price: 14,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg",
+  },
+  {
+    _id: "22",
+    name: "Fried Cauliflower",
+    image: food_22,
+    price: 22,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg",
+  },
+  {
+    _id: "23",
+    name: "Mix Veg Pulao",
+    image: food_23,
+    price: 10,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Pure Veg",
+  }
+];
